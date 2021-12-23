25f28b1198b1f0a45acb223126a0629f8b2d9d93
pstarlyk <bukoyesamuel9@gmail.com>
2021-12-23 23:00:31 +0100

    added hbs for dynamiv templating and made static html dynamic

--- src/app.js
+++ src/app.js
@@ -4,9 +4,27 @@ const app = express()
 const log = console.log
 
 const publicDirPath = path.join(__dirname, '../public')
-
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirPath))
 
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Samuel head'
+  })
+})
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Samuel head'
+  })
+})
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    message: 'This is something to help you'
+  })
+})
 app.get('/weather', (req, res) => {
   res.send('weather page')
 })
