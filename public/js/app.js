8cf5e32f34efe654100b45381350a4b77a96d4a1
pstarlyk <bukoyesamuel9@gmail.com>
2021-12-31 20:14:09 +0100

    created search input to receive location input and connected it to the fetch call

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,14 @@ const log = console.log
 
 log('thus is clint side js')
 
-fetch('http://localhost:3000/weather?address="!"')
-  .then(res => res.json())
-  .then(data => log(data))
-  .catch(err => log(err))
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const location = search.value
+  fetch(`http://localhost:3000/weather?address=${location}`)
+    .then(res => res.json())
+    .then(data => log(data))
+    .catch(err => log(err))
+})
