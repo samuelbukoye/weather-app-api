bde8edbd8c12fdd610d0d60f65dd917697a94bec
pstarlyk <bukoyesamuel9@gmail.com>
2022-01-03 16:13:57 +0100

    added dynamic port for heroku deployment

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for express config
 const publicDirPath = path.join(__dirname, '../public')
@@ -79,6 +80,6 @@ app.get('*', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('server is up on port 3000')
+app.listen(port, () => {
+  console.log(`server is up on port ${port}`)
 })
