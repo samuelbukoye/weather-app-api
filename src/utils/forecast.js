7f95dc3ec86d0ad49c67a43997456f1e42ea23d2
pstarlyk <bukoyesamuel9@gmail.com>
2022-01-03 17:06:28 +0100

    added sunrise and sunset time weather info from forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,14 @@
 const axios = require('axios').default
 
+function ConvertToTime (timestamp) {
+  var date = new Date(timestamp * 1000)
+  var hours = date.getHours()
+  var minutes = ('0' + date.getMinutes()).slice(-2)
+  var seconds = ('0' + date.getSeconds()).slice(-2)
+
+  return `${hours}:${minutes}`
+}
+
 const forecast = (lat, long, location, callback) => {
   const openweatherapiKey = '127d9e2cd99015fdd06f93737e4b535b'
   const openweatherapiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=metric&appid=${openweatherapiKey}`
@@ -10,7 +19,13 @@ const forecast = (lat, long, location, callback) => {
       const data = res.data
       callback(
         undefined,
-        `It is currently ${data.current.temp} degrees out in ${location}. With ${data.current.weather[0].description}`
+        `It is currently ${
+          data.current.temp
+        } degrees out in ${location}, with ${
+          data.current.weather[0].description
+        }. Sunrise at ${ConvertToTime(
+          data.current.sunrise
+        )}, and Sunset at ${ConvertToTime(data.current.sunset)}`
       )
     })
     .catch(function (err) {
